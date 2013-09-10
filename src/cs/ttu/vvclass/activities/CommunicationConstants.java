00e1c17a0e57c0d74c640cfd1a7b4e051f933391
Evgenii Balai <evgenii.balai@ttu.edu>
2013-09-10 03:04:33 -0500

    Add comments, rename packages

---
+++ src/cs/ttu/vvclass/activities/CommunicationConstants.java
@@ -0,0 +1,9 @@
+package cs.ttu.vvclass.activities;
+/**
+ * Constants used for communicating between the main activity and the activity displaying the results.
+ * result_i stands for the i-th number being factored
+ */
+public class CommunicationConstants {
+  public static final String [] extraDescriptions=
+		  new String[]{"result_1","result_2","result_3","result_4","result_5"};
+}
---
+++ src/cs/ttu/vvclass/activities/Pair.java
@@ -0,0 +1,20 @@
+
+package cs.ttu.vvclass.activities;
+
+/**
+ * Class to keep Pair of objects of two different types
+ * Used as a wrapper to pass parameters to FactorizationTask 
+ * @param <FIRST> the type(class) of the first object
+ * @param <SECOND> the type(class) of the seconds object
+ */
+public class Pair<FIRST, SECOND> {
+
+    public final FIRST first;
+    public final SECOND second;
+
+    /**Constructor**/
+    public Pair(FIRST first, SECOND second) {
+        this.first = first;
+        this.second = second;
+    }
+}
