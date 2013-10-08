7861fc0178ba85fd5d1a924c1ac3829fbfdf1247
Evgenii Balai <evgenii.balai@ttu.edu>
2013-10-08 12:41:29 -0500

    Fix bugs occurring in EP8 and EP12 test cases

--- Factorization/src/cs/ttu/vvclass/algorithms/PrimitiveDivision.java
+++ Factorization/src/cs/ttu/vvclass/algorithms/PrimitiveDivision.java
@@ -2,8 +2,10 @@ package cs.ttu.vvclass.algorithms;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
+
 /**
  * The class implements primitive division algorithm for number factorization
+ * 
  * @author Evgenii Balai
  * @author Cong Pu
  */
@@ -11,44 +13,47 @@ public class PrimitiveDivision implements FactorizationAlgo {
 
 	/**
 	 * Run primitive division algorithm
-	 * @param number to be factored
+	 * 
+	 * @param number
+	 *            to be factored
 	 * @return list of factors
 	 */
 	@Override
 	public ArrayList<BigInteger> run(BigInteger number) {
-		
-	    boolean  squareRootReached=false;
-	    BigInteger divisor=BigInteger.valueOf(2);
-	    ArrayList<BigInteger> factorization=new ArrayList<BigInteger>();
-	    
+
+		boolean squareRootReached = false;
+		BigInteger divisor = BigInteger.valueOf(1);
+		ArrayList<BigInteger> factorization = new ArrayList<BigInteger>();
+
 		// for one, no prime divisor exists:
 		if (number.equals(BigInteger.ONE)) {
 			factorization.add(BigInteger.ONE);
 			return factorization;
 		}
-	    // iterate through the divisors starting from two and
-	    // ending by the square root (rounded down) of the number 
-	    BigInteger originalNumber=number;
-	    while(!squareRootReached) {
-	       //check if we have a divisor and add it to factorization
-	       while(number.remainder(divisor).equals(BigInteger.ZERO)) {
-	    	   factorization.add(divisor);
-	    	   number=number.divide(divisor);
-	       }
-	       divisor=divisor.add(BigInteger.ONE);
-	       // check for reaching the square root
-	       // compute the square:
-	       BigInteger square=divisor.multiply(divisor);
-	       if(square.compareTo(originalNumber)>=0) {
-	    	   squareRootReached=true;
-	       }
-	    }
-	    
-	    // we may have a prime number which is greater than square root left
-	    if(number.compareTo(BigInteger.ONE)>0) {
-	    	factorization.add(number);
-	    }
-	    return factorization;
+		// iterate through the divisors starting from two and
+		// ending by the square root (rounded down) of the number
+		BigInteger originalNumber = number;
+		while (!squareRootReached) {
+			divisor = divisor.add(BigInteger.ONE);
+			// check for reaching the square root
+			// compute the square:
+			BigInteger square = divisor.multiply(divisor);
+			if (square.compareTo(originalNumber) >= 0) {
+				squareRootReached = true;
+			}
+			// check if we have a divisor and add it to factorization
+			while (number.remainder(divisor).equals(BigInteger.ZERO)) {
+				factorization.add(divisor);
+				number = number.divide(divisor);
+			}
+
+		}
+
+		// we may have a prime number which is greater than square root left
+		if (number.compareTo(BigInteger.ONE) > 0) {
+			factorization.add(number);
+		}
+		return factorization;
 	}
 
 }
